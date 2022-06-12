c3542f293484eea055d7ef8a2941e1520c80219e
SmokingCheese <107355494+SmokingCheese@users.noreply.github.com>
2022-06-12 15:37:59 +0200

    Add files via upload

---
+++ diviseur_propre.py
@@ -0,0 +1,18 @@
+def afficheMax(a,b):
+    if a==b:
+        print("they are the same")
+    elif a < b:
+        print(b,"is bigger than", a)
+    else:
+        print(a,"is bigger than", b)
+
+
+
+afficheMax(30,240)
+        
+       
+    
+                
+            
+
+
